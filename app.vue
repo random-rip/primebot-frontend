6ea76126d34dbcd34ecfdd4ea1d873b8dc63eebe
Robin Kehl <robin.kehl@singular-it.de>
2022-07-01 20:35:34 +0200

    remove icons (temp)

--- app.vue
+++ app.vue
@@ -1,33 +1,28 @@
 <template>
   <n-layout class="h-screen">
-    <n-layout-header bordered style="height: 64px; padding-left: 24px; padding-right: 24px">
+    <n-layout-header bordered style="height: 64px; padding-left: 24px; padding-right: 24px; position: fixed">
       <div class="flex flex-row justify-between w-full h-full">
         <span class="my-auto">PrimeBot</span>
         <n-menu mode="horizontal" :options="menuOptions" class="my-auto"/>
       </div>
     </n-layout-header>
-    <n-layout position="absolute" style="top: 64px; bottom: 64px" :native-scrollbar="false">
+    <n-layout position="absolute" style="top: 64px" :native-scrollbar="false">
       <n-layout-content content-style="padding: 24px">
         <NuxtPage/>
       </n-layout-content>
+      <n-layout-footer
+          style="height: 64px; padding: 24px"
+          bordered
+      >
+        Footer Footer Footer
+      </n-layout-footer>
     </n-layout>
-    <n-layout-footer
-        position="absolute"
-        style="height: 64px; padding: 24px"
-        bordered
-    >
-      Footer Footer Footer
-    </n-layout-footer>
   </n-layout>
+
 </template>
 
 <script setup lang="ts">
-import {NIcon} from 'naive-ui'
-import {
-  BookOutline as BookIcon,
-  PersonOutline as PersonIcon,
-  WineOutline as WineIcon
-} from '@vicons/ionicons5'
+import {NIcon} from "naive-ui";
 
 function renderIcon(icon: any) {
   return () => h(NIcon, null, {default: () => h(icon)})
@@ -35,75 +30,58 @@ function renderIcon(icon: any) {
 
 const menuOptions = [
   {
-    label: 'Home',
+    label: 'Startseite',
     key: 'home',
+    // icon: renderIcon(HomeIcon)
   },
   {
-    label: 'Get Started',
-    key: 'pinball-1973',
-    icon: renderIcon(BookIcon),
-    disabled: true,
+    label: 'Installation',
+    key: 'get-started',
+    // icon: renderIcon(MLIcon),
     children: [
       {
-        label: 'Rat',
-        key: 'rat'
+        label: 'Discord',
+        key: 'discord',
+        // icon: renderIcon(DiscordIcon),
+      },
+      {
+        label: 'Telegram',
+        key: 'telegram',
+        // icon: renderIcon(TelegramIcon),
       }
     ]
   },
   {
-    label: 'A Wild Sheep Chase',
-    key: 'a-wild-sheep-chase',
-    disabled: true,
-    icon: renderIcon(BookIcon)
-  },
-  {
-    label: 'Dance Dance Dance',
-    key: 'Dance Dance Dance',
-    icon: renderIcon(BookIcon),
+    label: 'Infos',
+    key: 'information',
+    // icon: renderIcon(InfoIcon),
     children: [
       {
-        type: 'group',
-        label: 'People',
-        key: 'people',
-        children: [
-          {
-            label: 'Narrator',
-            key: 'narrator',
-            icon: renderIcon(PersonIcon)
-          },
-          {
-            label: 'Sheep Man',
-            key: 'sheep-man',
-            icon: renderIcon(PersonIcon)
-          }
-        ]
+        label: 'Kontakt',
+        key: 'contact',
+        // icon: renderIcon(ContactIcon),
       },
       {
-        label: 'Beverage',
-        key: 'beverage',
-        icon: renderIcon(WineIcon),
-        children: [
-          {
-            label: 'Whisky',
-            key: 'whisky'
-          }
-        ]
+        label: 'Teams',
+        key: 'teams',
+        // icon: renderIcon(TeamIcon)
       },
       {
-        label: 'Food',
-        key: 'food',
-        children: [
-          {
-            label: 'Sandwich',
-            key: 'sandwich'
-          }
-        ]
+        label: 'Changelog',
+        key: 'changelog',
+        // icon: renderIcon(ChangelogIcon)
       },
       {
-        label: 'The past increases. The future recedes.',
-        key: 'the-past-increases-the-future-recedes'
+        label: 'Status',
+        key: 'status',
+        // icon: renderIcon(StatusIcon)
+      },
+      {
+        label: 'Open Source',
+        key: 'open-source',
+        // icon: renderIcon(GithubIcon)
       }
     ]
-  }
+  },
 ]
 </script>
