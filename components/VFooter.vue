3da76ed67aeb86507f27b7137752af81ebb38286
Robin Kehl <robin.kehl@singular-it.de>
2022-07-25 11:55:00 +0200

    feat: add footer links

---
+++ components/VFooter.vue
@@ -0,0 +1,17 @@
+<template>
+  <footer>
+    <n-space justify="center" size="medium">
+      <NuxtLink :to="link.to" v-for="link in links" :key="link.to"><n-text depth="3">{{link.text}}</n-text></NuxtLink>
+    </n-space>
+  </footer>
+</template>
+
+<script setup lang="ts">
+import {NSpace, NText} from 'naive-ui'
+
+const links = [
+  {text: 'Impressum', to: '/imprint'},
+  {text: 'Kontakt', to: '/information/contact'},
+  {text: 'Nutzungsbedingungen', to: '/privacy'},
+]
+</script>
