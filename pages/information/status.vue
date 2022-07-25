51dc9c2c44b2f4d3e87f55fa5e8c00d4878ba5f2
Robin Kehl <robin.kehl@singular-it.de>
2022-07-25 13:24:10 +0200

    feat: add status page

--- pages/information/status.vue
+++ pages/information/status.vue
@@ -1,3 +1,21 @@
 <template>
-  <n-h1>STATUS</n-h1>
+  <n-h1>Status</n-h1>
+  <n-space vertical>
+    <n-card>
+      <n-result :status="status.discord_status ? 'success' : 'error'" title="Discord Bot"></n-result>
+    </n-card>
+    <n-card>
+      <n-result :status="status.telegram_status ? 'success' : 'error'" title="Telegram Bot"></n-result>
+    </n-card>
+    <n-card>
+      <n-result :status="status.prime_league_status ? 'success' : 'error'" title="Primeleague API"></n-result>
+    </n-card>
+  </n-space>
 </template>
+
+<script setup lang="ts">
+import {NH1, NSpace, NResult, NCard} from 'naive-ui'
+import {useLazyFetch} from "#app";
+
+const { pending, data: status } = useLazyFetch('https://www.primebot.me/api/status/')
+</script>
