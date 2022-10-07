678e3497d7a2f454b96babbd0248343ffe2cf2ae
Robin Kehl <robin.kehl@singular-it.de>
2022-10-07 17:40:11 +0200

    fix: build

--- components/VPlatform.vue
+++ components/VPlatform.vue
@@ -1,23 +1,25 @@
 <template>
-  <ContentQuery :path="`${path}/meta`" v-slot="{ data }" find="one">
-    <n-h1>{{ data.title }}</n-h1>
-  </ContentQuery>
-  <ContentDoc :path="`/features`" v-slot="{ doc }">
-    <n-h2>{{ doc.title }}</n-h2>
-    <ContentRenderer :value="doc" />
-  </ContentDoc>
-  <ContentDoc :path="`${path}/first-steps`" v-slot="{ doc }">
-    <n-h2>{{ doc.title }}</n-h2>
-    <ContentRenderer :value="doc" />
-  </ContentDoc>
-  <ContentDoc :path="`${path}/commands`" v-slot="{ doc }">
-    <n-h2>{{ doc.title }}</n-h2>
-    <ContentRenderer :value="doc" />
-  </ContentDoc>
-  <ContentQuery :path="`${path}/faqs`" v-slot="{ data }">
-    <n-h2>FAQ</n-h2>
-    <v-faqs :data="data"></v-faqs>
-  </ContentQuery>
+  <main>
+    <ContentQuery :path="`${path}/meta`" v-slot="{ data }" find="one">
+      <n-h1>{{ data.title }}</n-h1>
+    </ContentQuery>
+    <ContentDoc :path="`/features`" v-slot="{ doc }">
+      <n-h2>{{ doc.title }}</n-h2>
+      <ContentRenderer :value="doc" />
+    </ContentDoc>
+    <ContentDoc :path="`${path}/first-steps`" v-slot="{ doc }">
+      <n-h2>{{ doc.title }}</n-h2>
+      <ContentRenderer :value="doc" />
+    </ContentDoc>
+    <ContentDoc :path="`${path}/commands`" v-slot="{ doc }">
+      <n-h2>{{ doc.title }}</n-h2>
+      <ContentRenderer :value="doc" />
+    </ContentDoc>
+    <ContentQuery :path="`${path}/faqs`" v-slot="{ data }">
+      <n-h2>FAQ</n-h2>
+      <v-faqs :data="data"></v-faqs>
+    </ContentQuery>
+  </main>
 </template>
 
 <script setup lang="ts">
