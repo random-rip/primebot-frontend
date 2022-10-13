3648b0d0693c5763101c6eeb68510b55f7fb08cc
Robin Kehl <robin.kehl@singular-it.de>
2022-10-13 13:44:39 +0200

    feat: finish settings

--- pages/information/status.vue
+++ pages/information/status.vue
@@ -1,23 +1,30 @@
 <template>
   <main>
-  <n-h1>Status</n-h1>
-  <n-space vertical>
-    <n-card>
-      <n-result :status="status.discord_status ? 'success' : 'error'" title="Discord Bot"></n-result>
-    </n-card>
-    <n-card>
-      <n-result :status="status.telegram_status ? 'success' : 'error'" title="Telegram Bot"></n-result>
-    </n-card>
-    <n-card>
-      <n-result :status="status.prime_league_status ? 'success' : 'error'" title="Primeleague API"></n-result>
-    </n-card>
-  </n-space>
+    <n-h1>Status</n-h1>
+        <n-space vertical>
+          <n-card>
+            <n-space justify="center">
+              Discord Bot <n-text :type="status?.discord_status ? 'success' : 'error'" strong>{{status?.discord_status ? 'online' : 'offline'}}</n-text>
+            </n-space>
+          </n-card>
+          <n-card>
+            <n-space justify="center">
+              Telegram Bot <n-text :type="status?.telegram_status ? 'success' : 'error'" strong>{{status?.telegram_status ? 'online' : 'offline'}}</n-text>
+            </n-space>
+          </n-card>
+          <n-card>
+            <n-space justify="center">
+              PrimeLeague Api <n-text :type="status?.prime_league_status ? 'success' : 'error'" strong>{{status?.prime_league_status ? 'online' : 'offline'}}</n-text>
+            </n-space>
+          </n-card>
+        </n-space>
   </main>
 </template>
 
 <script setup lang="ts">
-import {NH1, NSpace, NResult, NCard} from 'naive-ui'
-import {useLazyFetch} from "#app";
+import {NH1, NSpace, NCard, NText} from 'naive-ui'
+import {useLazyFetch} from "#imports";
+
 
 const { pending, data: status } = useLazyFetch('https://www.primebot.me/api/status/')
 </script>
---
+++ pages/settings.vue
@@ -0,0 +1,210 @@
+<template>
+  <main>
+    <n-space justify="center">
+      <n-image :src="settings?.logo_url" style="border-radius: 99999px"></n-image>
+    </n-space>
+    <n-space justify="space-between" align="center">
+      <n-h1>{{ settings?.team_name }} Einstellungen</n-h1>
+      <n-text strong>Läuft ab in:
+        <client-only>
+          <n-countdown :duration="(new Date(settings?.expiring_at).getTime()- new Date().getTime())"></n-countdown>
+        </client-only>
+      </n-text>
+    </n-space>
+    <n-form label-placement="top" label-width="auto">
+      <n-form-item label="Wöchentliche Benachrichtigung">
+        <n-switch v-model:value="weeklyNotification"></n-switch>
+      </n-form-item>
+      <n-form-item label="Benachrichtigung bei Lineup Änderungen">
+        <n-switch v-model:value="lineupNotification"></n-switch>
+      </n-form-item>
+      <n-form-item label="Benachrichtigung bei Terminvorschlägen von euch">
+        <n-switch v-model:value="teamSchedulingSuggestion"></n-switch>
+      </n-form-item>
+      <n-form-item label="Benachrichtigung bei Terminvorschlägen vom Gegner">
+        <n-switch v-model:value="enemySchedulingSuggestion"></n-switch>
+      </n-form-item>
+      <n-form-item label="Benachrichtigung bei Terminbestätigungen">
+        <n-switch v-model:value="schedulingConfirmation"></n-switch>
+      </n-form-item>
+      <n-form-item label="Benachrichtigung bei neuen Kommentaren (außer Teammitgliedern)">
+        <n-switch v-model:value="newComments"></n-switch>
+      </n-form-item>
+      <n-form-item label="Scoutingseite">
+        <n-select v-model:value="scouting" :options="scoutingOptions"></n-select>
+      </n-form-item>
+      <n-form-item label="Sprache">
+        <n-select v-model:value="language" :options="languageOptions"></n-select>
+      </n-form-item>
+      <client-only>
+        <n-button @click="submitSettings">Speichern</n-button>
+      </client-only>
+    </n-form>
+  </main>
+</template>
+
+<script setup>
+import {NH1, NCountdown, NSpace, NText, NImage, NForm, NFormItem, NSwitch, NSelect, NButton, useMessage} from 'naive-ui'
+import {useRoute} from "#app";
+import {computed, ref} from "#imports";
+import {useFetch} from "#app";
+
+const route = useRoute()
+
+const {
+  pending,
+  data: settings,
+} = useLazyFetch(`https://www.primebot.me/api/settings/?enc=${route.query.enc}&hash=${route.query.hash}&platform=${route.query.platform}`)
+
+const scouting = computed({
+  get() {
+    return settings.value?.settings.find((s) => {
+      return s.key === "SCOUTING_WEBSITE"
+    }).value
+  },
+  set(value) {
+    settings.value.settings.find((s) => {
+      return s.key === "SCOUTING_WEBSITE"
+    }).value = value
+  }
+})
+
+const scoutingOptions = [
+  {
+    label: 'op.gg',
+    value: 'op.gg'
+  },
+  {
+    label: 'u.gg',
+    value: 'u.gg'
+  },
+  {
+    label: 'xdx.gg',
+    value: 'xdx.gg'
+  }
+]
+
+const language = computed({
+  get() {
+    return settings.value?.settings.find((s) => {
+      return s.key === "LANGUAGE"
+    }).value
+  },
+  set(value) {
+    settings.value.settings.find((s) => {
+      return s.key === "LANGUAGE"
+    }).value = value
+  }
+})
+
+const languageOptions = [
+  {
+    label: 'deutsch',
+    value: 'de'
+  },
+  {
+    label: 'englisch',
+    value: 'en'
+  },
+]
+
+const message = useMessage()
+
+const submitting = ref(false)
+
+const submitSettings = async () => {
+  if(!submitting.value){
+    const {data, pending, error} = await useFetch('https://www.primebot.me/api/settings/', {
+      method: 'POST', body: {
+        ...settings.value,
+        enc: route.query.enc,
+        hash: route.query.hash,
+
+      }
+    })
+    submitting.value = pending
+    if (!error.value) {
+      message.info('Einstellungen wurden gespeichert')
+      settings.value = {...settings.value, ...data}
+    }
+  }
+}
+
+const weeklyNotification = computed({
+  get() {
+    return settings.value?.settings.find((s) => {
+      return s.key === "WEEKLY_MATCH_DAY"
+    }).value
+  },
+  set(value) {
+    settings.value.settings.find((s) => {
+      return s.key === "WEEKLY_MATCH_DAY"
+    }).value = value
+  }
+})
+
+const lineupNotification = computed({
+  get() {
+    return settings.value?.settings.find((s) => {
+      return s.key === "LINEUP_NOTIFICATION"
+    }).value
+  },
+  set(value) {
+    settings.value.settings.find((s) => {
+      return s.key === "LINEUP_NOTIFICATION"
+    }).value = value
+  }
+})
+const enemySchedulingSuggestion = computed({
+  get() {
+    return settings.value?.settings.find((s) => {
+      return s.key === "ENEMY_SCHEDULING_SUGGESTION"
+    }).value
+  },
+  set(value) {
+    settings.value.settings.find((s) => {
+      return s.key === "ENEMY_SCHEDULING_SUGGESTION"
+    }).value = value
+  }
+})
+
+const teamSchedulingSuggestion = computed({
+  get() {
+    return settings.value?.settings.find((s) => {
+      return s.key === "TEAM_SCHEDULING_SUGGESTION"
+    }).value
+  },
+  set(value) {
+    settings.value.settings.find((s) => {
+      return s.key === "TEAM_SCHEDULING_SUGGESTION"
+    }).value = value
+  }
+})
+
+const schedulingConfirmation = computed({
+  get() {
+    return settings.value?.settings.find((s) => {
+      return s.key === "SCHEDULING_CONFIRMATION"
+    }).value
+  },
+  set(value) {
+    settings.value.settings.find((s) => {
+      return s.key === "SCHEDULING_CONFIRMATION"
+    }).value = value
+  }
+})
+
+const newComments = computed({
+  get() {
+    return settings.value?.settings.find((s) => {
+      return s.key === "NEW_COMMENTS_OF_UNKNOWN_USERS"
+    }).value
+  },
+  set(value) {
+    settings.value.settings.find((s) => {
+      return s.key === "NEW_COMMENTS_OF_UNKNOWN_USERS"
+    }).value = value
+  }
+})
+
+</script>
